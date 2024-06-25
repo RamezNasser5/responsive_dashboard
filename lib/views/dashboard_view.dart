96e75f9f7fff1649ffad1bec9a438dacb24591e0
Ramez Nasser <rameznasser55@gmail.com>
2024-06-25 21:00:40 +0300

    Add desktop layout to DashboardView
    Introduced a new DashboardDesktopLayout widget to handle the desktop
    view of the dashboard. Updated DashboardView to use AdaptiveLayoutWidget
    for responsive design, providing different layouts for mobile, tablet,
    and desktop. This enhances the user experience by ensuring the dashboard
    is properly displayed across various devices.

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/dashboard_desktop_layout.dart';
+import 'package:responsive_dashboard/widget/adaptive_layout_widget.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayoutWidget(
+        mobileLayout: (context) => const Center(child: Text('Mobile')),
+        tabletLayout: (context) => const Center(child: Text('Tablet')),
+        desktopLayout: (context) => const DashboardDesktopLayout(),
+      ),
+    );
   }
 }
